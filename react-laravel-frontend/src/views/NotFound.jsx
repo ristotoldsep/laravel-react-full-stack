857fff31d9b14868c22c5d272b04f22ff6490380
Risto Tõldsep <r.toldsep@gmail.com>
2023-01-30 02:14:48 +0200

    Project setup and started on front-end

---
+++ react-laravel-frontend/src/views/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3 style={{ textDecoration: "underline" }}>
+                404 - Page not found.
+            </h3>
+        </div>
+    );
+};
+
+export default NotFound;
