4e1e432b022179e81ec6d7a9cb0f8b05a57f16bd
Risto Tõldsep <r.toldsep@gmail.com>
2023-02-08 18:41:02 +0200

    Added axios and will start implementing laravel api

--- react-laravel-frontend/src/views/Login.jsx
+++ react-laravel-frontend/src/views/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
+        <h1 className="title">Login to your account</h1>
         <form onSubmit={onSubmit}>
            <input type="email" placeholder='Email' />
            <input type="password" placeholder='Password' />
--- react-laravel-frontend/src/views/Signup.jsx
+++ react-laravel-frontend/src/views/Signup.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import axiosClient from '../axios-client';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Signup = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const {setUser, setToken} = useStateContext();
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    }
+    // console.log(payload)
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        setUser(data.user)
+        setToken(data.token)
+      })
+      .catch(err => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      })
+  } 
+
   return (
-    <div>Signup</div>
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <h1 className="title">Create an account</h1>
+        <form onSubmit={onSubmit}>
+           <input ref={nameRef} type="text" placeholder='Full Name' />
+           <input ref={emailRef} type="email" placeholder='Email' />
+           <input ref={passwordRef} type="password" placeholder='Password' />
+           <input ref={passwordConfirmationRef} type="password" placeholder='Password Confirmation' />
+           <button className="btn btn-block">Signup</button>
+           <p className="message">
+            Already Registered? <Link to="/login">Login</Link>
+           </p>
+        </form>
+      </div>
+    </div>
   )
 }
 
